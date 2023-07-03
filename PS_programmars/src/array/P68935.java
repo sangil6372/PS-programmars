0a8b8d228649ae0a523a4cde74bc49d273eb76ac
박상일 <sangil6372@naver.com>
2023-07-03 14:44:52 +0900

    문제9_3진법 뒤집기-Lv1

---
+++ PS_programmars/src/array/P68935.java
@@ -0,0 +1,27 @@
+package array;
+
+public class P68935 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	 public static  int solution(int n) {
+	        int answer = 0;
+//	        3진법을 문자열로 바꾼다음 뒤집기
+	        
+	        String str = Integer.toString(n, 3);
+	        
+	        String reversed = new StringBuilder(str).reverse().toString();
+
+	        
+	        answer = Integer.parseInt(reversed, 3);
+//	        parseInt와 valueOf의 차이는? -> Int 반환 / Integer 반환
+//	  		NumberFormatException시 null객체 반환 여부가 좀 큰 듯
+	        
+	        
+	        return answer;
+	    }
+	
+}
