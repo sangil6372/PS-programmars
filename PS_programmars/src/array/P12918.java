e3094ae46a8e949c6ca72085a85627847f0ce2dd
박상일 <sangil6372@naver.com>
2023-07-03 15:31:33 +0900

    문제13_문자열 다루기 기본-Lv1

---
+++ PS_programmars/src/array/P12918.java
@@ -0,0 +1,26 @@
+package array;
+
+public class P12918 {
+
+	public static void main(String[] args) {
+
+	}
+
+	public boolean solution(String s) {
+
+//	        정규식을 통해서 매치하는지 판단
+//	        answer = s.matches("[0-9]{4}|[0-9]{6}");
+
+		if (s.length() != 4 && s.length() != 6)
+			return false;
+
+		for (char c : s.toCharArray()) {
+			if (!Character.isDigit(c)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+}
