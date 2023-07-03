887b891bf3323cfb762f46ef3624225d83061dda
박상일 <sangil6372@naver.com>
2023-07-03 15:16:12 +0900

    문제12_숫자 문자열과 영단어_Lv1

---
+++ PS_programmars/src/array/P81301.java
@@ -0,0 +1,26 @@
+package array;
+
+public class P81301 {
+
+	private static final String[] words = {
+			"zero", "one", "two", "three", "four", "five","six"
+			, "seven", "eight","nine"
+	};
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+	}
+
+	 public static int solution(String s) {
+	        int answer = 0;
+
+	        for (int i =0;i<words.length;i++) {
+	        	s=s.replace(words[i], Integer.toString(i));
+	        }
+	        answer = Integer.parseInt(s);
+	        
+	        return answer;
+	    }
+	
+}
