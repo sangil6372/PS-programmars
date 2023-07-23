9b906b326803e2ea9323e3fec82be3cee6c63aa1
박상일 <sangil6372@naver.com>
2023-07-23 10:48:05 +0900

    문제39_정수 삼각형-Lv3

--- PS_programmars/src/dp/P43105.java
+++ PS_programmars/src/dp/P43105.java
@@ -1,40 +1,33 @@
 package dp;
 
-
 public class P43105 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
+//	동적프로그래밍 문제가 메모이제이션을 꼭 사용해야 되는 건 아니다.
 
-//	
-	
-	
 	public int solution(int[][] triangle) {
-			
-		 for (int i = 1; i < triangle.length; i++) {
+//		 삼각형 위에서 아래로 한 줄씩 갱신해나가는 거임
+		for (int i = 1; i < triangle.length; i++) {
 //			 맨 왼쪽 
-			 triangle[i][0] += triangle[i-1][0];
-			 
+			triangle[i][0] += triangle[i - 1][0];
 //		  	 가운데 
-			 for(int j = 1;j<i;j++) {
-				 triangle[i][j] +=  + Math.max(triangle[i - 1][j], triangle[i - 1][j - 1]);
-			 }
-			 
+			for (int j = 1; j < i; j++) {
+				triangle[i][j] += +Math.max(triangle[i - 1][j], triangle[i - 1][j - 1]);
+			}
 //			 맨 오른 쪽
-			 triangle[i][i] += triangle[i-1][i-1]; 
-			 
-		 }
-		 
-//		 마지막 줄 최댓값 
-		 int answer = 0;
-	        
-	        for (int i = 0; i < triangle.length; i++) {
-	            answer = Math.max(answer, triangle[triangle.length - 1][i]);
-	        }
-	        
-	     return answer;
+			triangle[i][i] += triangle[i - 1][i - 1];
+
+		}
+
+//		 마지막 줄에서 최댓값 
+		int answer = 0;
+		for (int i = 0; i < triangle.length; i++) {
+			answer = Math.max(answer, triangle[triangle.length - 1][i]);
+		}
+		return answer;
 	}
-	
+
 }
